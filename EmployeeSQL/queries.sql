563f49a4fb1cf121f4f216a5200011afa7abdb41
Phil Quintanilla <pvquintanilla@gmail.com>
2020-11-10 19:20:43 -0700

    finished queries and corrected ERD to have correct data types and added primary keys

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -51,3 +51,36 @@ AND last_name LIKE 'B%';
 
 
 ----List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  dept_emp.dept_no,
+  departments.dept_name
+FROM employees
+LEFT JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments ON
+dept_emp.dept_no=departments.dept_no
+WHERE dept_name='Sales';
+
+
+----List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  dept_emp.dept_no,
+  departments.dept_name
+FROM employees
+LEFT JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments ON
+dept_emp.dept_no=departments.dept_no
+WHERE dept_name='Sales'
+OR dept_name = 'Development';
+
+
+----In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Last_Name_Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Last_Name_Count" DESC;
